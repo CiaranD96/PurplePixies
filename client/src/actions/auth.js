1176eebc8148496857aa26af3c9e08edb51a4d32
Ciaran <ciaran.donnachie@gmail.com>
2020-12-09 21:54:45 +0000

    Created react project. Added user registration, login and logout features and added state mamagement using redux

---
+++ client/src/actions/auth.js
@@ -0,0 +1,102 @@
+import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import { setAlert } from './alert';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+} from './types';
+
+/**
+ * @function loadUser
+ * @description load a user
+ */
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get('/api/auth');
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
+
+/**
+ * @function register
+ * @description registers a user
+ * @param {String} username - username
+ * @param {String} password - password
+ */
+export const register = ({ username, password }) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const body = JSON.stringify({ username, password });
+
+  try {
+    const res = await axios.post('/api/users', body, config);
+
+    dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({ type: REGISTER_FAIL });
+  }
+};
+
+/**
+ * @function login
+ * @description login a user
+ * @param {String} username - username
+ * @param {String} password - password
+ */
+export const login = ({ username, password }) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const body = JSON.stringify({ username, password });
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+
+    dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({ type: LOGIN_FAIL });
+  }
+};
+
+/**
+ * @function logout
+ * @description log out user and clear profile
+ */
+
+export const logout = () => (dispatch) => {
+  dispatch({ type: LOGOUT });
+};
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Purple Pixies</h1>
+          <p className='lead'>
+            Welcome to Purple Pixies, home to the World of Warcraft guild Purple
+            Pixie Slayers on Kilrogg-EU
+          </p>
+          <div className='button'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
